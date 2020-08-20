b45af3e19afd8c0da7aa16ed313178bcd7934621
brucesz <zsyuyizhang@gmail.com>
2020-08-20 11:22:08 +0800

    adding core predict transform logic

--- yolo3_with_torch/darknet.py
+++ yolo3_with_torch/darknet.py
@@ -9,9 +9,63 @@ import torch.nn.functional as F
 
 from torch.autograd import Variable
 
+import utils
+
 import numpy as np
 
-def create_modules(bl_list):
+
+class EmptyLayer(nn.Module):
+    def __init__(self):
+        super(EmptyLayer, self).__init__()
+
+
+class DetectionLayer(nn.Module):
+    def __init__(self, anchors):
+        super(DetectionLayer,self).__init__()
+        self.anchors = anchors
+
+
+class DarkNet(nn.Module):
+    def __init__(self, cfgfile):
+        super(DarkNet, self).__init__()
+        self.blocks = utils.parse_cfg(cfgfile)
+        self.net_info , self.module_list = create_modules(self.blocks)
+
+    def forward(self, x, CUDA):
+        modules  = self.blocks[1:]
+        outputs = {}
+        # cache output here to implement route and shortcut.
+        write = 0
+        for i , module in enumerate(modules):
+            module_t = (module['type'])
+            if module_t == "convolutional" or module_t == "upsample":
+                x = self.module_list[i](x)
+            if module_t == "route":
+                layers = module["layers"]
+                layers = [int(a) for a in layers]
+                if (layers[0]) > 0:
+                    # layer from current i.
+                    layers[0] = layers[0]-i
+                
+                if len(layers) == 1:
+                    x = outputs[i + (layers[0])]
+                else:
+                    if (layers[1]>0):
+                        layers[1] = layers[1]  - i
+                    map1 = outputs[i + layers[0]]
+                    map2 = outputs[i + layers[1]]
+
+                    x = torch.cat((map1, map2), 1)
+
+            elif module_t == "shortcut":
+                from_ = int(module["from"])
+                x = outputs[i-1] + outputs[i+from_]
+                
+
+                
+        
+
+def create_modules(blocks):
     net_info = blocks[0]
     module_list = nn.ModuleList()
     prev_filters = 3
@@ -23,4 +77,98 @@ def create_modules(bl_list):
         # check type
         # create new module
         # append to module_list
+        if x['type'] == "convolutional":
+            #Get the info about the layer
+            act = x['activation']
+            try:
+                batch_norm = int(x['batch_normalize'])
+                bias = False
+            except:
+                batch_norm = 0
+                bais = True
 
+            filters = int(x['filters'])
+            padding = int(x['pad'])
+            kernel_size = int(x['size'])
+            stride = int(x['stride'])
+            
+            if padding:
+                pad = (kernel_size - 1) // 2
+            else:
+                pad  = 0
+            #Add the conv layer.
+            conv = nn.Conv2d(prev_filters, filters,  kernel_size, 
+                stride, pad, bias = bias)
+
+            module.add_module("conv_{0}".format(index),conv)
+
+            if batch_norm:
+                bn = nn.BatchNorm2d(filters)
+                module.add_module("batch_norm_{0}".format(index),bn)
+
+            # check act
+            if act == "leaky":
+                actvn = nn.LeakyReLU(0.1, inplace = True)
+                module.add_module("leaky_{0}".format(index), actvn)
+            
+        elif (x['type'] == "upsample"):
+            stride  = int(x['stride'])
+            upsample = nn.Upsample(scale_factor=2, mode = "bilinear")
+            module.add_module("upsample_{}".format(index), upsample)
+
+        elif (x['type'] == "route"):
+            x['layers'] = x['layers'].split(",")
+            start = int(x['layers'][0])
+
+            try:
+                end = int(x['layers'][1])
+            except:
+                end = 0
+
+            if start > 0:
+                start = start - index
+                
+            if end > 0:
+                end = end - index
+            
+            route  = EmptyLayer()
+
+            module.add_module("route_{0}".format(index),route)
+            
+            if end < 0:
+                filters = output_filters[index + start]  \
+                      + output_filters[index + end]
+            else:
+                filters  = output_filters[index + start]
+        elif (x['type'] == "shortcut"):
+            shortcut = EmptyLayer()
+            module.add_module("shortcut_{}".format(index),shortcut)
+
+        elif (x['type'] == "yolo") :
+            mask = x['mask'].split(",")
+            mask = [int(x) for x in mask]
+
+            anchors = x['anchors'].split(",")
+            anchors = [int(a) for a in anchors]
+            anchors = [(anchors[i] , anchors[i+1]) for i in range(0,len(anchors), 2)]
+            anchors = [anchors[i] for i in mask]
+
+            detection = DetectionLayer(anchors)
+            module.add_module("Detection_{}".format(index), detection)
+
+        module_list.append(module)
+        prev_filters = filters
+        output_filters.append(filters)
+    return (net_info, module_list)
+
+
+
+
+
+def test():
+    blocks = utils.parse_cfg("cfg/yolov3.cfg")
+    print(create_modules(blocks))
+
+
+if __name__ == "__main__":
+    test()
--- yolo3_with_torch/utils.py
+++ yolo3_with_torch/utils.py
@@ -2,6 +2,64 @@
 # coding=utf-8
 
 
+import torch
+import torch.nn as nn
+
+import torch.nn.functional as F
+from torch.autograd import Variable
+import numpy as np
+import cv2
+
+def predict_transform(pred, inp_dim, anchors, num_classes, CUDA = False):
+    batch_size = pred.size(0)
+    stride = inp_dim // pred.size(2)
+    grid_size = inp_dim // stride
+    bbox_attrs = 5 + num_classes
+    num_anchors = len(anchors)
+
+    pred = pred.view(batch_size, bbox_attrs* num_anchors, grid_size * grid_size)
+    pred = pred.transpose(1,2).contiguous()
+    pred = pred.view(batch_size, grid_size * grid_size * num_anchors,bbox_attrs)
+    anchors = [(a(0)/stride, a[1]/stride) for a in anchors]
+
+    pred[:, : , 0] = torch.sigmoid([pred[:,:,0]])
+    pred[:, : , 1] = torch.sigmoid([pred[:,:,1]])
+    pred[:, : , 4] = torch.sigmoid([pred[:,:,4]])
+
+    # add the center offsets
+    grid = np.arange(grid_size)
+    a, b = np.meshgrid(grid, grid)
+    x_off = torch.FloatTensor(a).view(-1,1)
+    y_off = torch.FloatTensor(b).view(-1,1)
+
+    if CUDA:
+        x_off = x_off.cuda()
+        y_off = y_off.cuda()
+
+    x_y_off = torch.cat((x_off, y_off),1).repeat(1, num_anchors).view(-1,2).unsqueeze(0)
+    pred[:,:,:2] += x_y_off
+
+
+    # add anchors to bbox
+    anchors = torch.FloatTensor(anchors)
+    if CUDA:
+        anchors = anchors.cuda()
+
+    anchors = anchors.repeat(grid_size * grid_size, 1).unsqueeze(0)
+    pred[:,:,2:4] = torch.exp(pred[:,:,2:4])*anchors
+
+    # compute class  score
+    pred[:,:,5:5+num_classes] = torch.sigmoid(pred[:,:,5:5 + num_classes])
+
+
+    # resize feature map to input_image size.
+    pred[:,:,:4] *= stride
+
+    return pred
+
+    
+
+
 def parse_cfg(conf):
     """
         Parse the cfg and 
