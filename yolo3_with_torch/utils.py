5c37d80567800c679f1226ac6864918cfb4e3210
brucesz <zsyuyizhang@gmail.com>
2020-09-04 15:32:38 +0800

    darknet code done, not working

--- yolo3_with_torch/utils.py
+++ yolo3_with_torch/utils.py
@@ -10,6 +10,116 @@ from torch.autograd import Variable
 import numpy as np
 import cv2
 
+
+class EmptyLayer(nn.Module):
+    def __init__(self):
+        super(EmptyLayer, self).__init__()
+
+
+class DetectionLayer(nn.Module):
+    def __init__(self, anchors):
+        super(DetectionLayer,self).__init__()
+        self.anchors = anchors
+
+
+def create_modules(blocks):
+    net_info = blocks[0]
+    module_list = nn.ModuleList()
+    prev_filters = 3
+    output_filters = []
+
+    for index, x in enumerate(blocks[1:]):
+        module = nn.Sequential()
+
+        # check type
+        # create new module
+        # append to module_list
+        if x['type'] == "convolutional":
+            #Get the info about the layer
+            act = x['activation']
+            try:
+                batch_norm = int(x['batch_normalize'])
+                bias = False
+            except:
+                batch_norm = 0
+                bais = True
+
+            filters = int(x['filters'])
+            padding = int(x['pad'])
+            kernel_size = int(x['size'])
+            stride = int(x['stride'])
+            
+            if padding:
+                pad = (kernel_size - 1) // 2
+            else:
+                pad  = 0
+            #Add the conv layer.
+            conv = nn.Conv2d(prev_filters, filters,  kernel_size, 
+                stride, pad, bias = bias)
+
+            module.add_module("conv_{0}".format(index),conv)
+
+            if batch_norm:
+                bn = nn.BatchNorm2d(filters)
+                module.add_module("batch_norm_{0}".format(index),bn)
+
+            # check act
+            if act == "leaky":
+                actvn = nn.LeakyReLU(0.1, inplace = True)
+                module.add_module("leaky_{0}".format(index), actvn)
+            
+        elif (x['type'] == "upsample"):
+            stride  = int(x['stride'])
+            upsample = nn.Upsample(scale_factor=2, mode = "bilinear")
+            module.add_module("upsample_{}".format(index), upsample)
+
+        elif (x['type'] == "route"):
+            x['layers'] = x['layers'].split(",")
+            start = int(x['layers'][0])
+
+            try:
+                end = int(x['layers'][1])
+            except:
+                end = 0
+
+            if start > 0:
+                start = start - index
+                
+            if end > 0:
+                end = end - index
+            
+            route  = EmptyLayer()
+
+            module.add_module("route_{0}".format(index),route)
+            
+            if end < 0:
+                filters = output_filters[index + start]  \
+                      + output_filters[index + end]
+            else:
+                filters  = output_filters[index + start]
+        elif (x['type'] == "shortcut"):
+            shortcut = EmptyLayer()
+            module.add_module("shortcut_{}".format(index),shortcut)
+
+        elif (x['type'] == "yolo") :
+            mask = x['mask'].split(",")
+            mask = [int(x) for x in mask]
+
+            anchors = x['anchors'].split(",")
+            anchors = [int(a) for a in anchors]
+            anchors = [(anchors[i] , anchors[i+1]) for i in range(0,len(anchors), 2)]
+            anchors = [anchors[i] for i in mask]
+
+            detection = DetectionLayer(anchors)
+            module.add_module("Detection_{}".format(index), detection)
+
+        module_list.append(module)
+        prev_filters = filters
+        output_filters.append(filters)
+    return (net_info, module_list)
+
+
+
 def predict_transform(pred, inp_dim, anchors, num_classes, CUDA = False):
     batch_size = pred.size(0)
     stride = inp_dim // pred.size(2)
@@ -17,14 +127,18 @@ def predict_transform(pred, inp_dim, anchors, num_classes, CUDA = False):
     bbox_attrs = 5 + num_classes
     num_anchors = len(anchors)
 
+    print("input pred.")
+    print(type(pred))
+
+    print("batch is: " + str(batch_size) + " ft: " + str(bbox_attrs * num_anchors) + "; grid:" + str(grid_size*grid_size))
     pred = pred.view(batch_size, bbox_attrs* num_anchors, grid_size * grid_size)
     pred = pred.transpose(1,2).contiguous()
     pred = pred.view(batch_size, grid_size * grid_size * num_anchors,bbox_attrs)
-    anchors = [(a(0)/stride, a[1]/stride) for a in anchors]
+    anchors = [(a[0]/stride, a[1]/stride) for a in anchors]
 
-    pred[:, : , 0] = torch.sigmoid([pred[:,:,0]])
-    pred[:, : , 1] = torch.sigmoid([pred[:,:,1]])
-    pred[:, : , 4] = torch.sigmoid([pred[:,:,4]])
+    pred[:, : , 0] = torch.sigmoid(pred[:,:,0])
+    pred[:, : , 1] = torch.sigmoid(pred[:,:,1])
+    pred[:, : , 4] = torch.sigmoid(pred[:,:,4])
 
     # add the center offsets
     grid = np.arange(grid_size)
